72a6af84ffd71dd8e178b790784a201c1155c78a
TRAN QUANG THIEN <trqt01646708@gmail.com>
2026-01-07 10:17:09 +0700

    bai2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/user/{id}', function ($id) {
+    $users = [
+		[
+			'id' => 1,
+			'name' => 'Trần Văn A',
+			'gender' => 'Nam'
+		],
+		[
+			'id' => 2,
+			'name' => 'Trần Văn B', 
+			'gender' => 'Nữ'
+		],
+        [
+            'id' => 3,
+            'name' => 'Trần Văn C', 
+            'gender' => 'Nam'
+        ]
+    ];
+
+    // Tìm user theo id
+    $user = null;
+    foreach ($users as $u) {
+        if ($u['id'] == $id) {
+            $user = $u;
+            break;
+        }
+    }
+
+    return view('user', compact('user'));
 });
